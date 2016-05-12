4b07489a22305ffa3511e0f8544c4c8b07909adc
Humberto Calero <altairn5@uga.edu>
2016-05-12 16:06:00 -0700

    new image background

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 			console.log("albums coming back = ", albums);
 			albums.forEach(function(album){
 
-				// renderHBS(albums);
+				renderHBS(albums);
 			});
 		}
 
@@ -17,3 +17,11 @@ $(document).ready(function(){
 });
 
 // Function takes a single album and renders it to the paged
+
+
+function renderHBS(data){
+	console.log("Rendering data for = ", JSON.stringify(data));
+
+
+
+}
